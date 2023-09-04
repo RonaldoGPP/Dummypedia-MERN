92ee902c321f5d9623ce35208301bd3860920e37
RonaldoGPP <ronaldo.prayogo@gmail.com>
2023-09-04 09:21:42 +0700

    deploy

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -37,7 +37,7 @@ const MyPostWidget = ({ picturePath }) => {
             formData.append("picturePath", image.name);
         }
 
-        const response = await fetch(`http://localhost:3001/posts`, {
+        const response = await fetch(`https://dummypedia-mern-backend.vercel.app/posts`, {
             method: "POST",
             headers:{ Authorization: `Bearer ${token}`},
             body: formData,
