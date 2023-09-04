86451bfbdbcf4fb36549bb9cb96192e241f8f8a1
RonaldoGPP <ronaldo.prayogo@gmail.com>
2023-09-04 11:19:35 +0700

    vercel

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use(cors({
-  origin: 'https://dummypedia-mern.vercel.app/'
+  origin: 'https://dummypedia-mern.vercel.app'
 }));
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
